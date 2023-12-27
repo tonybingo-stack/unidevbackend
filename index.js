5ab782419d03339f70216519e500b88745425bf2
tonybingo-stack <talentdev423@gmail.com>
2023-12-27 22:30:27 +0200

    added git igrnore

---
+++ index.js
@@ -0,0 +1,69 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const audioFilePath = './audio.mp3'; // Replace with the actual path to your MP3 file
+
+app.get('/stream-audio', (req, res) => {
+  const stat = fs.statSync(audioFilePath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = (end - start) + 1;
+    const file = fs.createReadStream(audioFilePath, { start, end });
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'audio/mp3',
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'audio/mp3',
+    };
+
+    res.writeHead(200, head);
+    fs.createReadStream(audioFilePath).pipe(res);
+  }
+});
+
+app.use(express.static('public'));
+
+app.get('/stream-text', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  const intervalId = setInterval(() => {
+    res.write(`data: ${new Date().toLocaleTimeString()}\n\n`);
+  }, 1000);
+
+  // Send initial message
+  res.write('data: Initial message\n\n');
+
+  // Close connection after 10 seconds (for demo purposes)
+  setTimeout(() => {
+    clearInterval(intervalId);
+    res.end();
+  }, 10000);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
