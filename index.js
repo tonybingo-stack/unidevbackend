7bfd40d1d829ea7a3ad5f83cf7cfe427b69765b8
tonybingo-stack <talentdev423@gmail.com>
2023-12-29 01:32:54 +0200

    update text stream

--- index.js
+++ index.js
@@ -22,15 +22,13 @@ app.get('/stream-text', (req, res) => {
   res.setHeader('Connection', 'keep-alive');
 
   // Send initial message
-  // res.write("data: initial message\n\n");
-  res.write("data: { data: [{percent: 30,shortText: 'Improved RO1 on ad spend', longDesc: 'Media buying helped with these results, since it's an important way of attracting clients', },\n\n");
-  res.write("data: {percent: 50, shortText: 'More engagement in majority of campains', longDesc: 'Ads generated significantly more engagement',},\n\n");
-  res.write("data: { percent: 38, shortText: 'Surge in Website Traffic', longDesc: 'As a consequence, website visits skyrocketed, boosted by social media engagement',},\n\n");
-  res.write("data: {percent: 42,shortText: Surge in Social Channel Traffic',longDesc: 'The Surge extended to social media, where engagement almost exactly followed website visits',},]}\n\n");
-  
+  res.write(`data: [{"percent": 30, "shortText": "Improved RO1 on ad spend", "longDesc": "Media buying helped with these results, since it's an important way of attracting clients"},\n\n`);
+  res.write(`data: {"percent": 50, "shortText": "More engagement in majority of campaigns", "longDesc": "Ads generated significantly more engagement"},\n\n`);
+  res.write(`data: {"percent": 38, "shortText": "Surge in Website Traffic", "longDesc": "As a consequence, website visits skyrocketed, boosted by social media engagement"},\n\n`);
+  res.write(`data: {"percent": 42, "shortText": "Surge in Social Channel Traffic", "longDesc": "The Surge extended to social media, where engagement almost exactly followed website visits"}]\n\n`);
+
   // Close connection after 10 seconds (for demo purposes)
   setTimeout(() => {
-    // clearInterval(intervalId);
     res.end();
   }, 10000);
 });
