8d291f028e9bd6bcc2072e0ec5d6b8ed879b8225
tonybingo-stack <talentdev423@gmail.com>
2024-01-04 09:57:25 +0200

    stream to socket

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 // server.js
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws');
 const fs = require('fs');
-const path = require('path');
 const cors = require('cors');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-const audioFilePath = './audio.mp3'; // Replace with the actual path to your MP3 file
-
+const io = socketIO(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 app.use(cors());
 
+const audioFilePath = './audio.mp3'; // Replace with the actual path to your MP3 file
+const data = `[
+  {"percent": 30, "shortText": "Improved RO1 on ad spend", "longDesc": "Media buying helped with these results, since it's an important way of attracting clients"},
+  {"percent": 50, "shortText": "More engagement in majority of campaigns", "longDesc": "Ads generated significantly more engagement"},
+  {"percent": 38, "shortText": "Surge in Website Traffic", "longDesc": "As a consequence, website visits skyrocketed, boosted by social media engagement"},
+  {"percent": 42, "shortText": "Surge in Social Channel Traffic", "longDesc": "The Surge extended to social media, where engagement almost exactly followed website visits"}
+]`;
 app.use(express.static('public'));
 
-app.get('/stream-text', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
+// app.get('/stream-text', (req, res) => {
+//   res.setHeader('Content-Type', 'text/event-stream');
+//   res.setHeader('Cache-Control', 'no-cache');
+//   res.setHeader('Connection', 'keep-alive');
 
-  // Send initial message
-  res.write(`data: [{"percent": 30, "shortText": "Improved RO1 on ad spend", "longDesc": "Media buying helped with these results, since it's an important way of attracting clients"},\n\n`);
-  res.write(`data: {"percent": 50, "shortText": "More engagement in majority of campaigns", "longDesc": "Ads generated significantly more engagement"},\n\n`);
-  res.write(`data: {"percent": 38, "shortText": "Surge in Website Traffic", "longDesc": "As a consequence, website visits skyrocketed, boosted by social media engagement"},\n\n`);
-  res.write(`data: {"percent": 42, "shortText": "Surge in Social Channel Traffic", "longDesc": "The Surge extended to social media, where engagement almost exactly followed website visits"}]\n\n`);
-
-  // Close connection after 10 seconds (for demo purposes)
-  setTimeout(() => {
-    res.end();
-  }, 10000);
-});
+//   // Send initial message
+//   res.write(`data: ${data} \n\n`);
+//   // Close connection after 10 seconds (for demo purposes)
+//   setTimeout(() => {
+//     res.end();
+//   }, 10000);
+// });
 
 
 app.get('/stream-audio', (req, res) => {
@@ -65,7 +70,23 @@ app.get('/stream-audio', (req, res) => {
   }
 });
 
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  
+  socket.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    if (message === 'data for finance card') {
+      console.log('sending...', data);
+      socket.send(data);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
